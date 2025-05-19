c84d639bdbef74a191c3ebd941bb50189cb4fac1
sina-tamadoni <ss.tamadoni@gmail.com>
2025-05-19 12:21:25 +0330

    react logo uncommented

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -9,9 +9,9 @@ function App() {
   return (
     <>
       <div>
-        {/* <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
+        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
